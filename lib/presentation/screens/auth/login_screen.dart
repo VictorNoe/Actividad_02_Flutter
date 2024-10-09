9d14fd1d1c60ad940fa38da57d67d49ca602e928
VictorNoe24 <noeflo60@gmail.com>
2024-10-09 11:58:50 -0600

    fix login

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -22,81 +22,83 @@ class _LoginScreenState extends State<LoginScreen> {
           padding: const EdgeInsets.all(32.0),
           child: Form(
             key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/image/logo.png',
-                  height: 200,
-                  width: 200,
-                ),
-                TextFormField(
-                  validator: validateEmail,
-                  decoration: const InputDecoration(
-                    hintText: 'Correo electrónico',
-                    label: Text('Correo Electrónico'),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(16.0))),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/image/logo.png',
+                    height: 200,
+                    width: 200,
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  validator: validatePasswordVoid,
-                  obscureText: _isObscure1,
-                  decoration: InputDecoration(
-                    hintText: 'Contraseña',
-                    label: const Text('Contraseña'),
-                    border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(16.0))),
-                    suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _isObscure1 = !_isObscure1;
-                          });
-                        },
-                        icon: Icon(_isObscure1
-                            ? Icons.visibility
-                            : Icons.visibility_off)),
+                  TextFormField(
+                    validator: validateEmail,
+                    decoration: const InputDecoration(
+                      hintText: 'Correo electrónico',
+                      label: Text('Correo Electrónico'),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  height: 48,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: OutlinedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8))),
-                    child: const Text('Iniciar sesión'),
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        
-                      }
-                    },
+                  const SizedBox(
+                    height: 20,
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(
-                          context, '/recovery_account');
-                    },
-                    child: const Text(
-                      'Recuperar contraseña',
-                      style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          color: Colors.black),
-                    ))
-              ],
+                  TextFormField(
+                    validator: validatePasswordVoid,
+                    obscureText: _isObscure1,
+                    decoration: InputDecoration(
+                      hintText: 'Contraseña',
+                      label: const Text('Contraseña'),
+                      border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _isObscure1 = !_isObscure1;
+                            });
+                          },
+                          icon: Icon(_isObscure1
+                              ? Icons.visibility
+                              : Icons.visibility_off)),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 48,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: OutlinedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8))),
+                      child: const Text('Iniciar sesión'),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          
+                        }
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(
+                            context, '/recovery_account');
+                      },
+                      child: const Text(
+                        'Recuperar contraseña',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.black),
+                      ))
+                ],
+              ),
             ),
           ),
         ),
