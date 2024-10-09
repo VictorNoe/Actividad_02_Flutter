096bd1fd4a50ec18b7ac23adb4d6778de444f224
VictorNoe24 <noeflo60@gmail.com>
2024-10-09 12:03:02 -0600

    add splash

---
+++ lib/presentation/screens/auth/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    }); 
+  }
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/image/logo.png',
+          width: 200,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
